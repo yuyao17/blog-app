2404fa954f3fd0c1ec3c51025f10335036567025
yuyao17 <yuya.oiwa@me.com>
2018-02-10 13:15:53 +0900

    added lots of stuff

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="post-list">
+      <PostPreview
+        id="1"
+        thumbnail="https://static.pexels.com/photos/248159/pexels-photo-248159.jpeg"
+        title="Hello there!"
+        previewText="This is my first post!" />
+      <PostPreview
+        id="2"
+        thumbnail="https://static.pexels.com/photos/248159/pexels-photo-248159.jpeg"
+        title="Hello there! - Second time"
+        previewText="This is my second post!" />
+      <PostPreview
+        id="3"
+        thumbnail="https://static.pexels.com/photos/248159/pexels-photo-248159.jpeg"
+        title="Hello there! - Third time"
+        previewText="This is my third post!" />
+    </section>
+</template>
+
+<script>
+
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+    components: {
+        PostPreview
+    }
+}
+
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
